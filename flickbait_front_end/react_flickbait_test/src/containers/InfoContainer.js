29f35b53b0a24f73a66e84e17f35aaf7f775db1c
Jack Cogan <jackcogan97@y7mail.com>
2022-02-27 15:19:34 +0000

    Made a pop-up menu and have the post methods working, somewhat

--- flickbait_front_end/react_flickbait_test/src/containers/InfoContainer.js
+++ flickbait_front_end/react_flickbait_test/src/containers/InfoContainer.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, isValidElement } from "react";
 import Rating from "../components/Rating";
 import "./InfoContainer.css"
 import Request from "../helpers/request";
@@ -23,25 +23,39 @@ const InfoContainer = () => {
       const handleWebPagePost = function(webPage){
         const request = new Request();
         request.post("http://www.localhost:8080/webpages", webPage)
-        .then(() => window.location.reload())
       }
 
       const handleVotePost = function(vote){
         const request = new Request();
         request.post("http://www.localhost:8080/votes", vote)
-        .then(() => window.location.reload())
+        // .then(() => window.location.reload())
       }
 
-      const handleCommentPost = function(comment){
+      const handleCommentPost = function(props){
         const request = new Request();
-        request.post("http://www.localhost:8080/comments", comment)
-        .then(() => window.location.reload())
+        request.post("http://www.localhost:8080/comments", props)
+        // .then(() => window.location.reload())
       }
 
-      const handleFullPost = (webpage, isUpVote, comment) => {
-        handleWebPagePost(webpage);
-        handleCommentPost(comment);
-        handleVotePost(isUpVote);
+      const handleFullPost = (webpage, vote, comment) => {
+        handleWebPagePost(webpage)
+          handleCommentPost(
+            {
+              "text": comment,
+              "webPage": {
+                "id": 1
+              }
+            }
+          )
+          handleVotePost(
+            {
+              "isUpVote": vote,
+              "webPage": {
+                "id": 1
+              }
+            }
+          )
+          .then(() => window.location.reload())
       }
 
       useEffect(()=>{
@@ -55,7 +69,8 @@ const InfoContainer = () => {
           {
             selectedWebPage && false
               ? <Rating selectedWebPage={selectedWebPage}/> 
-              : <Form url={url} urlLink={urlLink} handleFullPost={handleFullPost}/>
+              : <Form url={url} urlLink={urlLink} handleFullPost={handleFullPost}
+              handleComment={handleCommentPost}/>
           }
 
           <button onClick={() => handleCommentPost("Hello, World")}>A</button>
