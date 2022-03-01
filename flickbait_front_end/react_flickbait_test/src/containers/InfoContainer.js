c637c431081ca20bae395456f3f361b2627af226
Jack Cogan <jackcogan97@y7mail.com>
2022-03-01 14:20:38 +0000

    MVP???

--- flickbait_front_end/react_flickbait_test/src/containers/InfoContainer.js
+++ flickbait_front_end/react_flickbait_test/src/containers/InfoContainer.js
@@ -8,125 +8,135 @@ import $ from 'jquery';
 
 const InfoContainer = () => {
 
-    // const [webPages, setWebPages] = useState([]);
-    // const [comments, setComments] = useState([]);
-    const [votes, setVotes] = useState([]);
-    const [selectedWebPage, setSelectedWebPage] = useState([]);
-    const [latestVote, setLatestVote] = useState([]);
+  const today = new Date();
+  const date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+  const time = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
+  const dateTime = date + ' ' + time;
 
-    
-    let url = window.location.host;
-    let urlLink = window.location.pathname;
-    
-    const fullUrl = url + urlLink;
-    
-    const getUrl = function(){
-      fetch(`http://localhost:8080/webpages?url=${fullUrl}`)
-      .then(res => res.json())
-      .then(selectedWebPage123 => setSelectedWebPage(selectedWebPage123[0]))
+  // const [webPages, setWebPages] = useState([]);
+  // const [comments, setComments] = useState([]);
+  const [votes, setVotes] = useState([]);
+  const [selectedWebPage, setSelectedWebPage] = useState([]);
+  const [latestVote, setLatestVote] = useState([]);
+  
+  let url = window.location.host;
+  let urlLink = window.location.pathname;
+  
+  const fullUrl = url + urlLink;
+  
+  const getUrl = function(){
+    fetch(`http://localhost:8080/webpages?url=${fullUrl}`)
+    .then(res => res.json())
+    .then(selectedWebPage123 => setSelectedWebPage(selectedWebPage123[0]))
+  }
+  
+  const getVotes = function(){
+    fetch(`http://localhost:8080/votes/webpages?url=${fullUrl}`)
+    .then(res => res.json())
+    .then(voteList => setVotes(voteList))
+  }
+  
+  const handleWebPagePost = function(webPage){
+      const request = new Request();
+      request.post("http://www.localhost:8080/webpages", webPage)
+      .then(() => window.location.reload())
+    }
+    const handleWebPagePut = function(webpage) {
+      const request = new Request();
+      request.put(`http://www.localhost:8080/webpages/${selectedWebPage.id}`, webpage)
     }
     
-    const getVotes = function(){
-      fetch(`http://localhost:8080/votes/webpages?url=${fullUrl}`)
-      .then(res => res.json())
-      .then(voteList => setVotes(voteList))
+    const handleVotePost = function(vote){
+      const request = new Request();
+      request.post("http://www.localhost:8080/votes", vote)
+      .then(setLatestVote(vote))
+      // .then(() => window.location.reload())
     }
     
-    const handleWebPagePost = function(webPage){
-        const request = new Request();
-        request.post("http://www.localhost:8080/webpages", webPage)
-        .then(() => window.location.reload())
+    const handleCommentPost = function(props){
+      const request = new Request();
+      request.post("http://www.localhost:8080/comments", props)
+      // .then(() => window.location.reload())
+    }
+    
+    const addVote= (vote) => {
+      if (vote.upVote == true) {
+          selectedWebPage.upvotes += 1;
       }
+      else {
+          selectedWebPage.downvotes += 1;
+      }
+      const totalVotes = selectedWebPage.upvotes + selectedWebPage.downvotes;
+      const averageRating = (selectedWebPage.upvotes / totalVotes) * 100;
+      selectedWebPage.averageRating = averageRating;
+    }
 
-      const handleWebPagePut = function(webpage) {
-        const request = new Request();
-        request.put(`http://www.localhost:8080/webpages/${selectedWebPage.id}`, webpage)
-      }
-      
-      const handleVotePost = function(vote){
-        const request = new Request();
-        request.post("http://www.localhost:8080/votes", vote)
-        .then(setLatestVote(vote))
-        // .then(() => window.location.reload())
-      }
-      
-      const handleCommentPost = function(props){
-        const request = new Request();
-        request.post("http://www.localhost:8080/comments", props)
-        // .then(() => window.location.reload())
-      }
-      
-      const addVote= (vote) => {
-        if (vote.upVote == true) {
-            selectedWebPage.upvotes += 1;
-        }
-        else {
-            selectedWebPage.downvotes += 1;
-        }
-      }
+    const handleFullPost = (website, vote, comment) => {
+      // console.log(vote);
 
-      const handleFullPost = (website, vote, comment) => {
-        console.log(vote);
+      if (comment !== "") {
         handleCommentPost(
           {
             "text": comment,
+            "dateAndTime": dateTime,
             "webPage": {
               "id": selectedWebPage.id
             }
           }
-          )
-          handleVotePost(
-            {
-              "upVote": vote,
-              "webPage": {
-                "id": selectedWebPage.id
-              }
-            }
-          )
-          .then(() => window.location.reload())
+        )
       }
-      // useEffect(() => {
-      //   handleWebPagePost(fullUrl)
-      // },[])
 
-      useEffect(()=>{
-        getUrl()
-        console.log("first use effect triggered")
-      }, [])
-
-      useEffect(() => {
-        if(!selectedWebPage) {
-          console.log("second use effect triggered")
-          handleWebPagePost(fullUrl)
+      handleVotePost(
+        {
+          "upVote": vote,
+          "webPage": {
+            "id": selectedWebPage.id
+          }
         }
-        getVotes()
-      },[selectedWebPage])
+      )
+      .then(() => window.location.reload())
+    }
 
-      // useEffect(() => {
-      //   handleWebPagePut(selectedWebPage)
-      // },[votes])
+    // useEffect(() => {
+    //   handleWebPagePost(fullUrl)
+    // },[])
 
-      useEffect(() => {
-        addVote(latestVote) 
-        handleWebPagePut(selectedWebPage)
-      },[latestVote])
-      
-      
+    useEffect(()=>{
+      getUrl()
+      console.log("first use effect triggered")
+    }, [])
 
-    return (
-        <>
-        <HoverFunction/>
+    useEffect(() => {
+      if(!selectedWebPage) {
+        console.log("second use effect triggered")
+        handleWebPagePost(fullUrl)
+      }
+      getVotes()
+    },[selectedWebPage])
 
-          <div className="navbar-container"> 
-            {selectedWebPage && <Rating selectedWebPage={selectedWebPage}
-            votes={votes} url={fullUrl}/>}
-              <div className="form-container">
-                <Form url={url} urlLink={urlLink} handleFullPost={handleFullPost}
-                handleComment={handleCommentPost} selectedWebPage={selectedWebPage}/>
-              </div>
-          </div>
-        </>
-    )
+    // useEffect(() => {
+    //   handleWebPagePut(selectedWebPage)
+    // },[votes])
+
+    useEffect(() => {
+      addVote(latestVote) 
+      handleWebPagePut(selectedWebPage)
+    },[latestVote])
+    
+    
+  return (
+      <>
+      <HoverFunction/>
+        <div className="navbar-container"> 
+          {selectedWebPage && <Rating selectedWebPage={selectedWebPage}
+          votes={votes} url={fullUrl}/>}
+            <div className="form-container">
+              <Form url={url} urlLink={urlLink} handleFullPost={handleFullPost}
+              handleComment={handleCommentPost} selectedWebPage={selectedWebPage}/>
+            </div>
+        </div>
+      </>
+  )
 }
 
 export default InfoContainer;
