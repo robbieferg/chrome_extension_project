a8102c1fe8e4677eca314fdc9164895492f9b472
Robbie Ferguson <robbiejferguson92@gmail.com>
2022-03-01 17:55:29 +0000

    Fixed percentage rating display

--- flickbait_front_end/react_flickbait_test/src/components/Rating.js
+++ flickbait_front_end/react_flickbait_test/src/components/Rating.js
@@ -10,15 +10,19 @@ const Rating = ({selectedWebPage, votes, url}) => {
 
     const votesLength = votes.length;
 
-    // let ratingString = selectedWebPage.averageRating.toString();
+    let ratingString = selectedWebPage.averageRating + ""
+    console.log(ratingString)
+    const stringLength = 5;
 
-    // const stringLength = 5;
+    const percentageRating = ratingString.substring(0, stringLength);
+
+    
 
     return (
         <>
         <p className="block">Web URL: {selectedWebPage.url}</p>
         <p className="block"> No. of Votes: {votesLength}</p>
-        {/* <p className="block">{ratingString.substring(0, stringLength)}% of Users like this page</p> */}
+        <p className="block">{percentageRating}% of Users like this page</p>
         </>
     )
 }
