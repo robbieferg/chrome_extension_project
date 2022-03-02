96ba8cca16adce8cacc5ffe13f495c30b9405fd9
Jack Cogan <jackcogan97@y7mail.com>
2022-03-02 11:46:47 +0000

    Kinda got some extensions done

--- flickbait_front_end/react_flickbait_test/src/components/HoverFunction.js
+++ flickbait_front_end/react_flickbait_test/src/components/HoverFunction.js
@@ -3,68 +3,70 @@ import "./HoverFunction.css";
 // import Request from "../helpers/request";
 import $ from 'jquery';
 
-const HoverFunction = () => {
+const HoverFunction = ({logo, text}) => {
 
-    const [hoverHref, setHoverHref] = useState("");
-    const [link, setLink] = useState([{}]);
+  const [hoverHref, setHoverHref] = useState("");
+  const [link, setLink] = useState([{}]);
 
-    const trimHref = hoverHref.replace(/^https?\:\/\//i, "") ?? "";
+  const trimHref = hoverHref.replace(/^https?\:\/\//i, "") ?? "";
 
-    const ratingString = link?.[0]?.averageRating?.toString() ?? "" + "";
-    const stringLength = 5;
+  const ratingString = link?.[0]?.averageRating?.toString() ?? "" + "";
+  const stringLength = 5;
 
-    const percentageRating = ratingString?.substring(0, stringLength) ?? "No Data";
-    const numberOfVotes = (link?.[0]?.upvotes ?? 0) + (link?.[0]?.downvotes ?? 0);
+  const percentageRating = ratingString?.substring(0, stringLength) ?? "No Data";
+  const numberOfVotes = (link?.[0]?.upvotes ?? 0) + (link?.[0]?.downvotes ?? 0);
 
-    const getUrl = () => {
-      // console.log(`http://localhost:8080/webpages?url=${trimHref}`)
-      fetch(`http://localhost:8080/webpages?url=${trimHref}`)
-      .then(res => res.json())
-      .then(link => setLink(link));
-    }
+  const getUrl = () => {
+    // console.log(`http://localhost:8080/webpages?url=${trimHref}`)
+    fetch(`http://localhost:8080/webpages?url=${trimHref}`)
+    .then(res => res.json())
+    .then(link => setLink(link));
+  }
 
-    useEffect(() => {
-      getUrl()
-    }, [hoverHref])
+  useEffect(() => {
+    getUrl()
+  }, [hoverHref])
 
 
 
-    $(document).ready(function(){
+  $(document).ready(function(){
 
-          $(function() {
-            var moveLeft = 20;
-            var moveDown = 10;
-         
-            $('a').hover(function(e) {
+        $(function() {
+          var moveLeft = 20;
+          var moveDown = 10;
+        
+          $('a').hover(function(e) {
 
-                $('div#pop-up').show();
-                setHoverHref($(this).attr("href"))
-                // console.log(JSON.stringify($(this).attr("href")))
+              $('div#pop-up').show();
+              setHoverHref($(this).attr("href"))
 
-            }, function() {
+          }, function() {
 
-                $('div#pop-up').hide();
+              $('div#pop-up').hide();
 
-            });
-            $('a').mousemove(function(e) {
-              $("div#pop-up").css('top', e.pageY + moveDown).css('left', e.pageX + moveLeft);
-            });
           });
-    })
-    
-    return(
-        <>
-        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.5.0/jquery.min.js"></script>
+          $('a').mousemove(function(e) {
+            $("div#pop-up").css('top', e.pageY + moveDown).css('left', e.pageX + moveLeft);
+          });
+        });
+  })
+  
+  return(
+      <>
+      <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.5.0/jquery.min.js"></script>
 
-       
-            <div id="pop-up">
+      
+          <div id="pop-up">
+            <div id="top-bar">
+              <img id="hover-logo" src={logo} alt="flickbait-logo" />
+              <img id="flickbait-text" src={text} alt="flickbait" />
+            </div>
               <p>Total votes: {numberOfVotes}</p>
               <p>Avg. Rating: {percentageRating}%</p>
-              <p>Current Href:{hoverHref}</p>
-            </div>
-        
-      </>
-    )
+              {/* <p>Current Href:{hoverHref}</p> */}
+          </div>
+    </>
+  )
 }
 
 export default HoverFunction
--- flickbait_front_end/react_flickbait_test/src/components/Rating.js
+++ flickbait_front_end/react_flickbait_test/src/components/Rating.js
@@ -1,30 +1,43 @@
 
 import React from "react";
 
-const Rating = ({selectedWebPage, votes, url}) => {
-    
-    // const votesLength = votes.filter((vote) => {
-    //     return vote.webPage.url === url
-    // }
-    // );
+const Rating = ({ selectedWebPage, votes, comments, logo }) => {
 
-    const votesLength = votes.length;
+  // const votesLength = votes.filter((vote) => {
+  //     return vote.webPage.url === url
+  // }
+  // );
 
-    let ratingString = selectedWebPage.averageRating + ""
-    console.log(ratingString)
-    const stringLength = 5;
+  const votesLength = votes.length;
 
-    const percentageRating = ratingString.substring(0, stringLength);
+  let commentIndex = 0;
 
-    
+  let commentText = "";
 
-    return (
-        <>
-        <p className="block">Web URL: {selectedWebPage.url}</p>
-        <p className="block"> No. of Votes: {votesLength}</p>
-        <p className="block">{percentageRating}% of Users like this page</p>
-        </>
-    )
+  if (comments.length !== 0) {
+    commentIndex = comments.length - 1;
+    commentText = comments[commentIndex].text;
+  } else {
+    commentIndex = 0;
+    commentText = "No Comments Listed";
+  }
+
+  let ratingString = selectedWebPage.averageRating + ""
+  console.log(ratingString)
+  const stringLength = 5;
+
+  const percentageRating = ratingString.substring(0, stringLength);
+
+
+
+  return (
+    <>
+      <img id="logo" src={logo} alt="Logo" />
+      <p className="block"> No. of Votes: {votesLength}</p>
+      <p className="block">{percentageRating}% of Users like this page</p>
+      <p className="block">Latest Comment: {commentText}</p>
+    </>
+  )
 }
 
 export default Rating;
